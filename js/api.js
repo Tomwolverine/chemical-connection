78510fc007aa0666d7462d7567033b899a254f4d
Josh <joshaaronlevy@gmail.com>
2018-07-06 14:29:01 -0600

    Updated js files.

--- js/api.js
+++ js/api.js
@@ -1,156 +1,157 @@
-module.exports = [{
-  id:           1,
-  username:     'Josh Lovely',
-  faveElements: ['sodium', 'potassium', 'tantalum', 'gold']
-},
-                  {
-                    id:           2,
-                    username:     'George Chios',
-                    faveElements: ['aurelia', 'thorium', 'tantalum']
-                  },
-                  {
-                    id:           3,
-                    username:     'Natalie Todd',
-                    faveElements: ['ruthenium', 'ytterbium', 'tantalum']
-                  },
-                  {
-                    id:           4,
-                    username:     'Tom Cluckies',
-                    faveElements: ['ruthenium', 'gold', 'tantalum']
-                  },
-                  {
-                    id:           5,
-                    username:     'Eldon Dahlin',
-                    faveElements: ['ruthenium', 'nickel', 'radon']
-                  },
-                  {
-                    id:           6,
-                    username:     'Kevin Plotts',
-                    faveElements: ['sodium', 'iridium', 'radon']
-                  },
-                  {
-                    id:           7,
-                    username:     'Austin Loveless',
-                    faveElements: ['ruthenium', 'iridium', 'tantalum']
-                  },
-                  {
-                    id:           8,
-                    username:     'Madeline Kreamer',
-                    faveElements: ['xenon', 'iridium', 'iodine']
-                  },
-                  {
-                    id:           9,
-                    username:     'Hunt Applegate',
-                    faveElements: ['xenon', 'iridium', 'iodine']
-                  },
-                  {
-                    id:           10,
-                    username:     'Kris Kamikawa',
-                    faveElements: ['ruthenium', 'sodium', 'tantalum']
-                  },
-                  {
-                    id:           11,
-                    username:     'Crystal Preston-Watson',
-                    faveElements: ['gold', 'iridium', 'tantalum']
-                  },
-                  {
-                    id:           12,
-                    username:     'Ciaran Guha-Gilford',
-                    faveElements: ['ruthenium', 'helium', 'tantalum']
-                  },
-                  {
-                    id:           13,
-                    username:     'Josh Robbs',
-                    faveElements: ['ytterbium', 'iridium', 'tantalum']
-                  },
-                  {
-                    id:           14,
-                    username:     'Joey Walter',
-                    faveElements: ['ruthenium', 'helium', 'tantalum']
-                  },
-                  {
-                    id:           14,
-                    username:     'Nicholas Lee',
-                    faveElements: ['ruthenium', 'radon', 'tantalum']
-                  },
-                  {
-                    id:           15,
-                    username:     'Tiffany Ricks',
-                    faveElements: ['ruthenium', 'iridium', 'radon']
-                  },
-                  {
-                    id:           16,
-                    username:     'Matthew Riach',
-                    faveElements: ['helium', 'iridium', 'tantalum']
-                  },
-                  {
-                    id:           17,
-                    username:     'Brice Sinkule',
-                    faveElements: ['sodium', 'gold', 'tantalum']
-                  },
-                  {
-                    id:           18,
-                    username:     'Jose Stricklin',
-                    faveElements: ['xenon', 'iridium', 'tantalum']
-                  },
-                  {
-                    id:           19,
-                    username:     'Danny Pifer',
-                    faveElements: ['iodine', 'radon', 'tantalum']
-                  },
-                  {
-                    id:           20,
-                    username:     'Jeff Strunk',
-                    faveElements: ['xenon', 'iridium', 'tantalum']
-                  },
-                  {
-                    id:           21,
-                    username:     'Anh Nguyen',
-                    faveElements: ['ruthenium', 'radon', 'helium']
-                  },
-                  {
-                    id:           22,
-                    username:     'Jared Candelaria',
-                    faveElements: ['ruthenium', 'iridium', 'sodium']
-                  },
-                  {
-                    id:           23,
-                    username:     'Michael Coons',
-                    faveElements: ['ruthenium', 'helium', 'tantalum']
-                  },
-                  {
-                    id:           24,
-                    username:     'Tom Lee',
-                    faveElements: ['gold', 'iodine', 'tantalum']
-                  },
-                  {
-                    id:           25,
-                    username:     'Jen Rudnicki',
-                    faveElements: ['gold', 'radon', 'tantalum']
-                  },
-                  {
-                    id:           26,
-                    username:     'Levi Boenish',
-                    faveElements: ['ruthenium', 'helium', 'tantalum']
-                  },
-                  {
-                    id:           27,
-                    username:     'Ryan McCroy',
-                    faveElements: ['ruthenium', 'iridium', 'tantalum']
-                  },
-                  {
-                    id:           28,
-                    username:     'Jed Prentup',
-                    faveElements: ['ruthenium', 'xenon', 'tantalum']
-                  },
-                  {
-                    id:           29,
-                    username:     'Damon Chivers',
-                    faveElements: ['helium', 'iridium', 'tantalum']
-                  },
-                  {
-                    id:           30,
-                    username:     'Conrad Wright',
-                    faveElements: ['ytterbium', 'iridium', 'gold']
-                  }
+module.exports = [
+  {
+    id:           1,
+    username:     'Josh Lovely',
+    faveElements: ['sodium', 'potassium', 'tantalum', 'gold']
+  },
+  {
+    id:           2,
+    username:     'George Chios',
+    faveElements: ['aurelia', 'thorium', 'tantalum']
+  },
+  {
+    id:           3,
+    username:     'Natalie Todd',
+    faveElements: ['ruthenium', 'ytterbium', 'tantalum']
+  },
+  {
+    id:           4,
+    username:     'Tom Cluckies',
+    faveElements: ['ruthenium', 'gold', 'tantalum']
+  },
+  {
+    id:           5,
+    username:     'Eldon Dahlin',
+    faveElements: ['ruthenium', 'nickel', 'radon']
+  },
+  {
+    id:           6,
+    username:     'Kevin Plotts',
+    faveElements: ['sodium', 'iridium', 'radon']
+  },
+  {
+    id:           7,
+    username:     'Austin Loveless',
+    faveElements: ['ruthenium', 'iridium', 'tantalum']
+  },
+  {
+    id:           8,
+    username:     'Madeline Kreamer',
+    faveElements: ['xenon', 'iridium', 'iodine']
+  },
+  {
+    id:           9,
+    username:     'Hunt Applegate',
+    faveElements: ['xenon', 'iridium', 'iodine']
+  },
+  {
+    id:           10,
+    username:     'Kris Kamikawa',
+    faveElements: ['ruthenium', 'sodium', 'tantalum']
+  },
+  {
+    id:           11,
+    username:     'Crystal Preston-Watson',
+    faveElements: ['gold', 'iridium', 'tantalum']
+  },
+  {
+    id:           12,
+    username:     'Ciaran Guha-Gilford',
+    faveElements: ['ruthenium', 'helium', 'tantalum']
+  },
+  {
+    id:           13,
+    username:     'Josh Robbs',
+    faveElements: ['ytterbium', 'iridium', 'tantalum']
+  },
+  {
+    id:           14,
+    username:     'Joey Walter',
+    faveElements: ['ruthenium', 'helium', 'tantalum']
+  },
+  {
+    id:           14,
+    username:     'Nicholas Lee',
+    faveElements: ['ruthenium', 'radon', 'tantalum']
+  },
+  {
+    id:           15,
+    username:     'Tiffany Ricks',
+    faveElements: ['ruthenium', 'iridium', 'radon']
+  },
+  {
+    id:           16,
+    username:     'Matthew Riach',
+    faveElements: ['helium', 'iridium', 'tantalum']
+  },
+  {
+    id:           17,
+    username:     'Brice Sinkule',
+    faveElements: ['sodium', 'gold', 'tantalum']
+  },
+  {
+    id:           18,
+    username:     'Jose Stricklin',
+    faveElements: ['xenon', 'iridium', 'tantalum']
+  },
+  {
+    id:           19,
+    username:     'Danny Pifer',
+    faveElements: ['iodine', 'radon', 'tantalum']
+  },
+  {
+    id:           20,
+    username:     'Jeff Strunk',
+    faveElements: ['xenon', 'iridium', 'tantalum']
+  },
+  {
+    id:           21,
+    username:     'Anh Nguyen',
+    faveElements: ['ruthenium', 'radon', 'helium']
+  },
+  {
+    id:           22,
+    username:     'Jared Candelaria',
+    faveElements: ['ruthenium', 'iridium', 'sodium']
+  },
+  {
+    id:           23,
+    username:     'Michael Coons',
+    faveElements: ['ruthenium', 'helium', 'tantalum']
+  },
+  {
+    id:           24,
+    username:     'Tom Lee',
+    faveElements: ['gold', 'iodine', 'tantalum']
+  },
+  {
+    id:           25,
+    username:     'Jen Rudnicki',
+    faveElements: ['gold', 'radon', 'tantalum']
+  },
+  {
+    id:           26,
+    username:     'Levi Boenish',
+    faveElements: ['ruthenium', 'helium', 'tantalum']
+  },
+  {
+    id:           27,
+    username:     'Ryan McCroy',
+    faveElements: ['ruthenium', 'iridium', 'tantalum']
+  },
+  {
+    id:           28,
+    username:     'Jed Prentup',
+    faveElements: ['ruthenium', 'xenon', 'tantalum']
+  },
+  {
+    id:           29,
+    username:     'Damon Chivers',
+    faveElements: ['helium', 'iridium', 'tantalum']
+  },
+  {
+    id:           30,
+    username:     'Conrad Wright',
+    faveElements: ['ytterbium', 'iridium', 'gold']
+  }
 ];
---
+++ js/app.js
@@ -0,0 +1,47 @@
+(function initializePage() {
+  const apiUrl = 'https://chemical-connection.herokuapp.com/';
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(response => response.data)
+    .then(data => data.forEach(getNerd));
+})();
+
+function getNerd(nerd) {
+  const $li = document.createElement('li');
+  console.log(nerd);
+  const $elements = [
+    // getImage(nerd.id, nerd.username),
+    getNameplate(nerd.username, nerd.faveElements),
+    // getMessage(nerd.message),
+    // getLink(nerd.name)
+  ].forEach($element => $li.appendChild($element));
+  document.querySelector('#nerds').appendChild($li);
+}
+
+function getImage(id, altText) {
+  const $image = document.createElement('img');
+  $image.src = id;
+  $image.alt = altText;
+  return $image;
+}
+
+function getNameplate(username, faveElements) {
+  return getElement('span', `${username} - ${faveElements}`);
+}
+
+function getMessage(message) {
+  return getElement('p', message);
+}
+
+function getLink(name) {
+  const $a = getElement('a', `Leave ${name} a message`);
+  $a.href = `contact.html?nerd=${name}`;
+  return $a;
+}
+
+function getElement(tagName, text) {
+  const $element = document.createElement(tagName);
+  const $text = document.createTextNode(text);
+  $element.appendChild($text);
+  return $element;
+}
