64e60f3166380be805cbee0c45f6c61a1724d8ae
Josh <joshaaronlevy@gmail.com>
2018-07-06 14:16:52 -0600

    Added stuff

--- js/app.js
+++ js/app.js
@@ -9,9 +9,33 @@
 function getNerd(nerd) {
   const $li = document.createElement('li');
   const $elements = [
-    getNerd(nerd.id, nerd.username, nerd.faveElements)
+    // getImage(nerd.id, nerd.username),
+    getNameplate(nerd.id, nerd.username, nerd.faveElements),
+    // getMessage(nerd.message),
+    // getLink(nerd.name)
   ].forEach($element => $li.appendChild($element));
   document.querySelector('#nerds').appendChild($li);
+}
+
+function getImage(id, altText) {
+  const $image = document.createElement('img');
+  $image.src = id;
+  $image.alt = altText;
+  return $image;
+}
+
+function getNameplate(name, phoneNumber) {
+  return getElement('span', `${name} - ${phoneNumber}`);
+}
+
+function getMessage(message) {
+  return getElement('p', message);
+}
+
+function getLink(name) {
+  const $a = getElement('a', `Leave ${name} a message`);
+  $a.href = `contact.html?nerd=${name}`;
+  return $a;
 }
 
 function getElement(tagName, text) {
