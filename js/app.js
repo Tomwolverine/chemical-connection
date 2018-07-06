32327a2fbc68d65556b08a67490f144bb0c07891
Josh <joshaaronlevy@gmail.com>
2018-07-06 13:53:48 -0600

    Updated a bunch of stuff.

---
+++ js/app.js
@@ -0,0 +1,46 @@
+(function initializePage() {
+  const apiUrl = 'https://quiet-bayou-99554.herokuapp.com/api/v1/contacts';
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(response => response.data)
+    .then(data => data.forEach(getNerd));
+})();
+
+function getNerd(nerd) {
+  const $li = document.createElement('li');
+  const $elements = [
+    // getImage(nerd.id, nerd.username),
+    getNameplate(nerd.id, nerd.username, nerd.faveElements)
+    // getMessage(nerd.message),
+    // getLink(nerd.name)
+  ].forEach($element => $li.appendChild($element));
+  document.querySelector('#nerds').appendChild($li);
+}
+
+function getImage(id, altText) {
+  const $image = document.createElement('img');
+  $image.src = id;
+  $image.alt = altText;
+  return $image;
+}
+
+function getNameplate(name, phoneNumber) {
+  return getElement('span', `${name} - ${phoneNumber}`);
+}
+
+function getMessage(message) {
+  return getElement('p', message);
+}
+
+function getLink(name) {
+  const $a = getElement('a', `Leave ${name} a message`);
+  $a.href = `contact.html?nerd=${name}`;
+  return $a;
+}
+
+function getElement(tagName, text) {
+  const $element = document.createElement(tagName);
+  const $text = document.createTextNode(text);
+  $element.appendChild($text);
+  return $element;
+}
