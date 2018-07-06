b1e49e98971ffdabd8eaed8be0d44fb82a692934
Josh <joshaaronlevy@gmail.com>
2018-07-06 11:20:40 -0600

    Added api.js file.

---
+++ js/api.js
@@ -0,0 +1,17 @@
+module.exports = [{
+  'id':           1,
+  'username':     'Josh Lovely',
+  'faveElements': [
+    'sodium',
+    'potassium',
+    'chromium',
+    'gold'
+  ]
+}, {
+  'id':           2,
+  'username':     'George Chios',
+  'faveElements': [
+    'aurelia',
+    'thorium',
+  ]
+}]
